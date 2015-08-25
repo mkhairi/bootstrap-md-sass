840e674afcb05aaede75d19fee9249364d15eb44
mkhairi <khairi@labs.my>
2015-08-25 18:39:37 +0800

    update assets

--- lib/bootstrap-md-sass/engine.rb
+++ lib/bootstrap-md-sass/engine.rb
@@ -6,7 +6,7 @@ module Bootstrap
           %w(stylesheets javascripts fonts images).each do |sub|
             app.config.assets.paths << root.join('app/assets', sub).to_s
           end
-          app.config.assets.precompile << %r(MaterialIcons-Regular\.(?:eot|svg|ttf|woff2?)$)
+          app.config.assets.precompile << %r(material/MaterialIcons-Regular\.(?:eot|svg|ttf|woff2?)$)
         end
       end
     end
