476521b75461e0233324be52c7ff46cd40582007
Mohd Khairi Mohd Adnan <khairi@labs.my>
2015-09-22 17:18:27 +0800

    Update smooth-scrolling.js

--- app/assets/javascripts/bootstrap-md/smooth-scrolling.js
+++ app/assets/javascripts/bootstrap-md/smooth-scrolling.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
 	$(function() {
 
-		$('a[href*=#]:not([href=#])').click(function() {
+		$('.smooth-scroll a[href*=#]:not([href=#])').click(function() {
 
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
